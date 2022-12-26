135d472da9b1e3f6994f4765b752884c0017b182
Aldo <aldoft@hotmail.com>
2022-12-26 12:34:43 +0100

    day3, first test green

--- day3/rucksack/rucksack.go
+++ day3/rucksack/rucksack.go
@@ -1,1 +1,29 @@
 package rucksack
+
+import (
+	"errors"
+	"strings"
+)
+
+func GetCommonItemInRucksack(rucksuck string) (rune, error) {
+	size := len(rucksuck)
+	firstHalf := rucksuck[:size/2]
+	secondHalf := rucksuck[size/2:]
+
+	for _, item := range firstHalf {
+		if itemInCompartment(item, secondHalf) {
+			return item, nil
+		}
+	}
+
+	for _, item := range secondHalf {
+		if itemInCompartment(item, firstHalf) {
+			return item, nil
+		}
+	}
+	return rune(0), errors.New("no common item found")
+}
+
+func itemInCompartment(item rune, compartment string) bool {
+	return strings.ContainsRune(compartment, item)
+}
--- day3/rucksack/rucksack_test.go
+++ day3/rucksack/rucksack_test.go
@@ -6,22 +6,46 @@ import (
 	"github.com/AldoFusterTurpin/AdventOfCode-2022/day3/rucksack"
 )
 
-func TestGetCommonItemInCompartments(t *testing.T) {
+func TestGetCommonItemInRucksack(t *testing.T) {
 	type testData struct {
-		compartmensItems []string
-		expected         string
+		compartmensItems string
+		expected         rune
 	}
 
 	tests := map[string]testData{
 		"example_1": {
-			compartmensItems: []string{"vJrwpWtwJgWr", "hcsFMMfFFhFp"},
-			expected:         "p",
+			compartmensItems: "vJrwpWtwJgWrhcsFMMfFFhFp",
+			expected:         'p',
+		},
+		"example_2": {
+			compartmensItems: "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+			expected:         'L',
+		},
+		"example_3": {
+			compartmensItems: "PmmdzqPrVvPwwTWBwg",
+			expected:         'P',
+		},
+		"example_4": {
+			compartmensItems: "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+			expected:         'v',
+		},
+		"example_5": {
+			compartmensItems: "ttgJtRGJQctTZtZT",
+			expected:         't',
+		},
+		"example_6": {
+			compartmensItems: "CrZsJsPPZsGzwwsLwLmpwMDw",
+			expected:         's',
 		},
 	}
 
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
-			got := rucksack.GetCommonItemInCompartments(tc.compartmensItems)
+			got, err := rucksack.GetCommonItemInRucksack(tc.compartmensItems)
+			if err != nil {
+				t.Fatalf("expected no error, but got %v", err)
+			}
+
 			if tc.expected != got {
 				t.Fatalf("expected %v, but got %v", tc.expected, got)
 			}
